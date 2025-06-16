5a96944b916e88a859d0f2d1f981a91894c5a585
13354 <1335461738@qq.com>
2025-06-16 10:52:46 +0800

    修改初始化错误

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue'),
     },
     {
       path: '/about',
